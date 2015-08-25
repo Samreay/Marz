db22ecf1e672c42b7d275dd75bf0603b67be438d
Samuel Hinton <samuelreay@gmail.com>
2015-08-25 17:44:14 +1000

    Adding scripts for local server starting

---
+++ runServer.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+echo Starting server at http://localhost:8080
+python -m SimpleHTTPServer 8080
+pause
---
+++ runServer.sh
@@ -0,0 +1,2 @@
+echo "Starting server at http://localhost:8080"
+python -m SimpleHTTPServer 8080
