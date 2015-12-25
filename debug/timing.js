b342a848c2201e5d9460e108019f9619ce573736
Samuel <samuelreay@gmail.com>
2015-12-25 01:21:17 -0300

    Adding comparison method, to try and balance changing CPU utilisation.

--- debug/timing.js
+++ debug/timing.js
@@ -8,7 +8,8 @@ var debug = function(output) {
 
 debug("Loading dependancies");
 var fs = require('fs');
-var dependencies = ['./js/config.js', './lib/fits.js', './js/tools.js', './js/methods.js', './lib/dsp.js', './lib/regression.js', './js/templates.js', './js/classes.js', './js/workerMethods.js'];
+var dependencies = ['./js/config.js', './lib/fits.js', './js/tools.js', './js/methods.js', './lib/dsp.js', './lib/regression.js', './js/templates.js', './js/classes.js'];
+//dependencies.push('./js/workerMethods.js');
 for (var i = 0; i < dependencies.length; i++) {
     eval(fs.readFileSync("../" + dependencies[i]) + '');
 }
@@ -16,7 +17,7 @@ debug("Dependencies loaded\n");
 var node = true;
 
 var inputMessage = require('./input.js');
-var size = 20000;
+var size = 5000000;
 var randomArray = new Array(size);
 var randomFloat = new Float64Array(size);
 for (var i = 0; i < size; i++) {
@@ -26,7 +27,7 @@ for (var i = 0; i < size; i++) {
 
 inputMessage.matching = false;
 var timeFunction = function(name, func, num) {
-    num = defaultFor(num, 100);
+    num = defaultFor(num, 200);
     var times = [];
     for (var i = 0; i < num; i++) {
         var t = new Date();
@@ -38,12 +39,42 @@ var timeFunction = function(name, func, num) {
     var std = getRMS(times);
     debug(name + " took " + mean.toFixed(0) + " pm " + std.toFixed(0));
 };
+var compareFunc = function(name, funcs, num) {
+    num = defaultFor(num, 200);
+    var times = new Array(funcs.length);
+    for (var i = 0; i < times.length; i++) {
+        times[i] = [];
+    }
+    for (var i = 0; i < num; i++) {
+        for (var j = 0; j < funcs.length; j++) {
+            var t = new Date();
+            funcs[j]();
+            var e = new Date() - t;
+            times[j].push(e);
+        }
+    }
+    for (var j = 0; j < funcs.length; j++) {
+        var mean = getMean(times[j]);
+        var std = getRMS(times[j]);
+        debug(name + " " + j + " took " + mean.toFixed(1) + " pm " + std.toFixed(1));
+    }
+};
 
+var singleTests = {};
+var doubleTests = {};
 
-var tests = {};
-tests.handleEvent = function() { handleEvent(inputMessage); };
+singleTests.getMean = function() { getMean(randomArray); };
 
-debug("Starting tests");
-for (var key in tests) {
-    timeFunction(key, tests[key]);
+doubleTests.compareMeans = [function() { getMean(randomArray); }, function() { getMean(randomArray); }];
+
+debug("Starting single tests");
+for (var key in singleTests) {
+    timeFunction(key, singleTests[key]);
+}
+
+debug("\nStarting comparison tests");
+for (var key in doubleTests) {
+    debug("--------------------");
+    debug(key);
+    compareFunc(key, doubleTests[key]);
 }
