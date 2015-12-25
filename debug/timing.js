245c8cc2198d1f97c6b1c2377ca4b5bbd6b5f573
Samuel <samuelreay@gmail.com>
2015-12-25 01:04:00 -0300

    Fixing skeleton

--- debug/timing.js
+++ debug/timing.js
@@ -6,34 +6,44 @@ var debug = function(output) {
     }
 };
 
-debug("Loading dependancies\n");
+debug("Loading dependancies");
 var fs = require('fs');
-var dependencies = ['./js/config.js', './lib/fits.js', './js/tools.js', './js/methods.js', './lib/regression.js', './js/templates.js', './js/classes.js'];
+var dependencies = ['./js/config.js', './lib/fits.js', './js/tools.js', './js/methods.js', './lib/dsp.js', './lib/regression.js', './js/templates.js', './js/classes.js', './js/workerMethods.js'];
 for (var i = 0; i < dependencies.length; i++) {
     eval(fs.readFileSync("../" + dependencies[i]) + '');
 }
+debug("Dependencies loaded\n");
+var node = true;
 
 var inputMessage = require('./input.js');
+var size = 20000;
+var randomArray = new Array(size);
+var randomFloat = new Float64Array(size);
+for (var i = 0; i < size; i++) {
+    randomArray[i] = 100 * Math.random();
+    randomFloat[i] = randomArray[i];
+}
 
-
-var timeFunction = function(name, func) {
+inputMessage.matching = false;
+var timeFunction = function(name, func, num) {
+    num = defaultFor(num, 100);
     var times = [];
-    var i = 0;
-    for (i = 0; i < 100; i++) {
+    for (var i = 0; i < num; i++) {
         var t = new Date();
         func();
-        times.push(new Date() - t);
+        var e = new Date() - t;
+        times.push(e);
     }
     var mean = getMean(times);
     var std = getRMS(times);
-    debug(name + " took " + mean.toFixed(1) + " pm " + std.toFixed(1));
+    debug(name + " took " + mean.toFixed(0) + " pm " + std.toFixed(0));
 };
 
+
 var tests = {};
+tests.handleEvent = function() { handleEvent(inputMessage); };
 
-tests.medianSmall = function() { medianFilter(inputMessage.intensity, 10); };
-tests.medianLarge = function() { medianFilter(inputMessage.intensity, 100); };
-
+debug("Starting tests");
 for (var key in tests) {
     timeFunction(key, tests[key]);
 }
