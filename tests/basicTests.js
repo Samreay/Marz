1679b45c5550d3cb083a947538dd8c9ca54d2c3f
Samuel <samuelreay@gmail.com>
2015-12-26 22:09:35 -0300

    Adding some basic mean and stddev tests

--- tests/basicTests.js
+++ tests/basicTests.js
@@ -11,10 +11,47 @@ for (var i = 0; i < dependencies.length; i++) {
 }
 console.log("Dependencies loaded\n");
 
-
+var testArray = [3,5,4,2,5,3,6,3,9,5,3,2,4,3,5,3,8,7,7,2,4];
 var tests = [];
 
-
+/**
+ * Tests for basic functions like mean and stdDev
+ */
+tests.push({
+    name: "getMean test",
+    fn: function() {
+        return getMean(testArray)
+    },
+    expected: 4.4285714285714288});
+tests.push({
+    name: "getMeanMask test",
+    fn: function() {
+        var mask = [];
+        for (var i = 0; i < testArray.length; i++) {
+            mask.push(testArray[i] > 4)
+        }
+        return getMeanMask(testArray, mask)
+    },
+    expected: 6.333333333333333});
+tests.push({
+    name: "getStdDev test",
+    fn: function() {
+        return getStdDev(testArray)
+    },
+    expected: 1.965692137195266});
+tests.push({
+    name: "getStdDevMask test",
+    fn: function() {
+        var mask = [];
+        for (var i = 0; i < testArray.length; i++) {
+            mask.push(testArray[i] > 4)
+        }
+        return getStdDevMask(testArray, mask)
+    },
+    expected: 1.4142135623730951});
+/**
+ *  Tests for the linked lists and median filter
+ */
 tests.push({
     name: "Linked List - addAllToSorted test",
     fn: function() {
@@ -44,25 +81,25 @@ tests.push({
 tests.push({
     name: "Linked List - medianFilter test (1)",
     fn: function() {
-        return medianFilter([3,5,4,2,5,3,6,3,9,5,3,2,4,3,5,3,8,7,7,2,4], 3);
+        return medianFilter(testArray, 3);
     },
     expected: [3, 4, 4, 4, 3, 5, 3, 6, 5, 5, 3, 3, 3, 4, 3, 5, 7, 7, 7, 4, 4]});
 tests.push({
     name: "Linked List - medianFilter test (2)",
     fn: function() {
-        return medianFilter([3,5,4,2,5,3,6,3,9,5,3,2,4,3,5,3,8,7,7,2,4], 5);
+        return medianFilter(testArray, 5);
     },
     expected: [3, 3, 4, 4, 4, 3, 5, 5, 5, 3, 4, 3, 3, 3, 4, 5, 7, 7, 7, 4, 4]});
 tests.push({
     name: "Linked List - medianFilter test (3)",
     fn: function() {
-        return medianFilter([3,5,4,2,5,3,6,3,9,5,3,2,4,3,5,3,8,7,7,2,4], 7);
+        return medianFilter(testArray, 7);
     },
     expected: [3, 3, 3, 4, 4, 4, 5, 5, 3, 4, 3, 4, 3, 3, 4, 5, 5, 5, 4, 4, 4]});
 tests.push({
     name: "Linked List - medianFilter test (4)",
     fn: function() {
-        return medianFilter([3,5,4,2,5,3,6,3,9,5,3,2,4,3,5,3,8,7,7,2,4], 9);
+        return medianFilter(testArray, 9);
     },
     expected: [3, 3, 3, 3, 4, 5, 4, 3, 4, 3, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4]});
 
