f730177a6265cb2a30e92b20c349edcbe92dd904
Samuel <samuelreay@gmail.com>
2015-12-29 12:40:14 -0300

    Closing #120. Also making Marz command line accept directories or multiple files. Adding in numCPUs to defaults to allow finer control

--- autoConfig.js
+++ autoConfig.js
@@ -1,17 +1,24 @@
 var defaults = {};
 
-/** Properties I don't expect to be changed */
+/** Marz output configuration */
+defaults.debug = false;               // Whether to print debug output
+
+defaults.outFile = null;              // The output filename. A null value means use the original filename, such that abc.fits -> abc.mz
+defaults.dir = null;                  // Output directory. A null value means output in the same directory as the fits file. Path is relative to input path
+// NOTE: If neither outFile nor dir is supplied, Marz only outputs to the console (so you can redirect output)
+// WARNING: Giving Marz a directory as path input and specifying a specific outFile name will not end well for you
+
+
+/** Matching formatting and algorithm control */
+defaults.numAutomatic = 3;            // How many automatic matches to return
+defaults.disabledTemplates = [];      // List of template IDs to disable in matching, eg to disable only quasars set to ['12']
+defaults.numCPUs = null;              // A null value defaults to using all but one logical CPUs on the host machine
+
+
+/** Properties that should not be changed. Modify at your own risk. */
 defaults.assignAutoQOPs = true;       // Whether or not assign autoQops
 defaults.processTogether = true;      // To process and match together. Should be enable for automatic matching
 
 
-/** Properties I expect to be changed */
-defaults.numAutomatic = 3;            // How many automatic matches to return
-defaults.tenabled = [];               // List of template IDs to disable in matching, eg to disable only quasars set to ['12']
-
-
-
-
-
-/** Make available to node or iojs */
+/** Make available to node */
 module.exports = defaults;
