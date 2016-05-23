b0ed7ecfdeef17b4a4d9887abd95f95468047308
Simon Conseil <contact@saimon.org>
2016-05-23 12:12:35 +0200

    Remove prettyprint residuals

--- js/directives.js
+++ js/directives.js
@@ -90,14 +90,6 @@ angular.module('directivesZ', ['servicesZ', 'ngSanitize'])
             }
         }
     }])
-    .directive('prettyprint', function() {
-        return {
-            restrict: 'C',
-            link: function postLink(scope, element, attrs) {
-                element.html(prettyPrintOne(element.html()));
-            }
-        };
-    })
     .directive('keybind', function() {
 
         return {
